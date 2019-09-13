779845f680c63fa65c9b84da5a3600caf6294ef2
mountainrambler <mountainrambler@gmail.com>
2019-09-13 16:36:03 +0200

    Removed some code

--- script.py
+++ script.py
@@ -6,26 +6,9 @@ Created on Fri Sep 13 12:46:16 2019
 @author: peternystrom
 """
 
-import math
-import os
-import sys
 
 import requests
 
-
-# print(sys.version)
-print("It starts here")
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    #    test = "test"
-    return greeting
-
-
-print(greet("World"))
-print(greet("Peter"))
 
 r = requests.get("http://www.industrivarden.se")
 print(r.status_code)
